3dd96b1952c26b954e7829bd9ce5a8114b058cf4
tigerabrodi <tigerabrodi@gmail.com>
2023-03-10 05:49:00 +0100

    schemas

---
+++ app/schemas/index.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+
+export type ValueOf<T> = T[keyof T]
+
+export const contentNames = {
+  headline: 'Headline',
+  text: 'Text',
+  twitter: 'Twitter',
+  linkedin: 'LinkedIn',
+} as const
+
+export type ContentNameValue = ValueOf<typeof contentNames>
+
+export const messageSchema = z.object({
+  role: z.string(),
+  content: z.string(),
+})
+
+export const choiceSchema = z.object({
+  message: messageSchema,
+  finish_reason: z.string(),
+  index: z.number(),
+})
