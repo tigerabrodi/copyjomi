6790ed1a59137e0a7dda257bc496822025be0bb4
tigerabrodi <tigerabrodi@gmail.com>
2023-03-13 08:13:14 +0100

    metatags

--- app/root.tsx
+++ app/root.tsx
@@ -24,8 +24,25 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'Copyjomi',
-  description: '',
+  description: 'A tool to help with writing better content.',
   viewport: 'width=device-width,initial-scale=1',
+  keywords:
+    'copywriting, content, writing, write, better, ai, chatgpt, copyjomi',
+  'og:title': 'Copyjomi',
+  'og:type': 'website',
+  'og:url': 'https://www.copyjomi.com/',
+  'og:image':
+    'https://user-images.githubusercontent.com/49603590/224631291-18ceaa9e-4797-4a9d-a00a-f5fe8516b28f.png',
+  'og:card': 'summary_large_image',
+  'og:creator': '@tabrodi',
+  'og:site': 'https://www.copyjomi.com/',
+  'og:description': 'A tool to help with writing better content.',
+  'twitter:image':
+    'https://user-images.githubusercontent.com/49603590/224631291-18ceaa9e-4797-4a9d-a00a-f5fe8516b28f.png',
+  'twitter:card': 'summary_large_image',
+  'twitter:creator': '@tabrodi',
+  'twitter:title': 'Copyjomi',
+  'twitter:description': 'A tool to help with writing better content.',
 })
 
 export default function App() {
